951ff20c55554d2d6c7fbe945e0fdaa17e20f44d
Raigyo <vincent.chilot@gmail.com>
2019-06-03 16:33:21 +0200

    update readme

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/tasks', 'TaskController@all')->name('tasks.all');
+
+Route::post('/tasks', 'TaskController@store')->name('tasks.store');
+
+Route::get('/tasks/{task}', 'TaskController@show')->name('tasks.show');
+
+Route::put('/tasks/{task}', 'TaskController@update')->name('tasks.update');
+
+Route::delete('/tasks/{task}', 'TaskController@destory')->name('tasks.destroy');
